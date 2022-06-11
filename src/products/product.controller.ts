a837a6a58a9ad41565e7bad857279ad0bd73510e
Haziz Khan <hazizkhan1999@gmail.com>
2022-06-12 04:41:43 +0500

    created get discount of the product api

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { ProductService } from './product.service';
 
 @Controller('product')
@@ -8,5 +9,25 @@ export class ProductController {
   @Get()
   getHello(): string {
     return 'hello from product controller';
+  }
+
+  @Get('discount/:id')
+  async getDiscount(@Res() res: Response, @Param('id') id: string) {
+    try {
+      const discountDetails = await this.productsService.getDiscount(id);
+      if (!discountDetails || !discountDetails.length) {
+        res.status(404).send('Product not found!');
+        return;
+      }
+      return res.send({
+        data: discountDetails[0].discount
+          ? discountDetails[0].discount * 100
+          : -1,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send('Invalid response!');
+      return;
+    }
   }
 }
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Product, ProductType } from './product.schema';
 @Injectable()
 export class ProductService {
@@ -15,5 +15,58 @@ export class ProductService {
 
   async addMany(docs: Array<Product>) {
     return this.productModel.insertMany(docs);
+  }
+
+  async getDiscount(productId: string) {
+    const id = new Types.ObjectId(productId);
+    return this.productModel.aggregate([
+      {
+        $match: { _id: id },
+      },
+      {
+        $addFields: {
+          categoryId: {
+            $cond: {
+              if: { $gte: ['$discount', 0] },
+              then: null,
+              else: '$categoryId',
+            },
+          },
+        },
+      },
+      {
+        $graphLookup: {
+          from: 'categories',
+          startWith: '$categoryId',
+          connectFromField: 'parentId',
+          connectToField: '_id',
+          as: 'hierarchy',
+          depthField: 'level',
+        },
+      },
+      {
+        $addFields: {
+          el: {
+            $filter: {
+              input: '$hierarchy',
+              as: 'item',
+              cond: { $gt: ['$$item.discount', 0] },
+            },
+          },
+        },
+      },
+      { $sort: { 'el.level': -1 } },
+      {
+        $project: {
+          discount: {
+            $cond: {
+              if: { $gte: ['$discount', 0] },
+              then: '$discount',
+              else: { $arrayElemAt: ['$el.discount', 0] },
+            },
+          },
+        },
+      },
+    ]);
   }
 }
